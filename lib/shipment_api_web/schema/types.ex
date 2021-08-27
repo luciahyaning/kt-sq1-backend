34753c48edceced6abf6a45449041f4c6edf7cd2
luciahyaning <luciahyaningpergiwatiputri.kinasih@mitrais.com>
2021-08-27 14:49:58 +0800

    prepare the schema type

---
+++ lib/shipment_api_web/schema/types.ex
@@ -0,0 +1,11 @@
+defmodule ShipmentApiWeb.Schema.Types do
+  use Absinthe.Schema.Notation
+
+  alias ShipmentApiWeb.Schema.Types
+
+  # import_types(Types.Usertype)
+  # import_types(Types.SessionType)
+
+  # import_types(Types.ListType)
+  # import_types(Types.TaskType)
+end

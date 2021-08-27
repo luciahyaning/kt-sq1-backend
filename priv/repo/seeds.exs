35a8aa49ebf21a1e33b07a9a2bcd239d0d83762d
Stephani Angelina <stephani.angelina91@gmail.com>
2021-08-27 13:31:50 +0700

    create ecto seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,18 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Repo.insert! %users{id: 1, role: "Transporter"}
+Repo.insert! %users{id: 2, role: "Shipper"}
+
+Repo.insert! %truck_types{id: 1, name: "Tronton"}
+Repo.insert! %truck_types{id: 2, name: "Container"}
+Repo.insert! %truck_types{id: 3, name: "CDE"}
+
+Repo.insert! %shipment_status{id: 1, name: "Assigned"}
+Repo.insert! %shipment_status{id: 2, name: "Allocated"}
+Repo.insert! %shipment_status{id: 3, name: "Ongoing to Origin"}
+Repo.insert! %shipment_status{id: 4, name: "At Origin"}
+Repo.insert! %shipment_status{id: 5, name: "Ongoing to Destination"}
+Repo.insert! %shipment_status{id: 6, name: "At Destination”}
+Repo.insert! %shipment_status{id: 7, name: "Completed"}
